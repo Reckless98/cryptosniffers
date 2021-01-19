423182cac04accba162f6b205a11764944d1683b
reckless98 <filipwolf@gmail.com>
2021-01-19 16:57:48 +0100

    xx

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="main">
+      <h4>Footer</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="nav-bar">
+      <h1>Header</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
